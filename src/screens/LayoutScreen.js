0c7a5c57c31ab185d1757a9c462c494396b81998
Björn Bergquist <bjorn.bergquist@codic.se>
2021-11-15 12:01:25 +0100

    Did lecture on useWindowDimensions

--- src/screens/LayoutScreen.js
+++ src/screens/LayoutScreen.js
@@ -1,15 +1,34 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, useWindowDimensions } from 'react-native'
+import testStyle from '../shared/utils/testStyle'
 
 const LayoutScreen = () => {
-    return (
-        <View style={styles.parent}>
-            <View style={styles.child1}></View>
-            <View style={styles.child2}></View>
-            <View style={styles.child3}></View>
-            <View style={styles.child4}></View>
-        </View>
-    )
+    const window = useWindowDimensions()
+    const landscape = window.width > window.height
+
+    const display = () =>{
+        if (landscape) {
+            return <Text>Landscape</Text>
+        } else {
+            return ( <Text> Test </Text>
+           )  
+        }
+    }
+
+    return ( <View style={{...testStyle.background, ...styles.parent}}>
+    {/* <View style={styles.child1}></View>
+    <View style={styles.child2}></View>
+    <View style={styles.child3}></View>
+    <View style={styles.child4}></View> */}
+    <Text>Height: {window.height}</Text>
+    <Text>Width: {window.width}</Text>
+    <Text>Scale: {window.scale}</Text>
+    <Text>FontScale: {window.fontScale}</Text>
+    <Text>Aspect-ratio: {window.height/window.width}</Text>
+    <Text>Aspect-ratio: {16/9}</Text>
+    <Text style={landscape?styles.landscape:styles.portrait}>{landscape?"Landscape orientation":"Portrait orientation"}</Text>
+  
+</View> )
 }
 
 export default LayoutScreen
@@ -17,15 +36,27 @@ export default LayoutScreen
 const styles = StyleSheet.create({
     parent:{
         flex: 1,
-        flexDirection: "row",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
     }, 
+    parentWide:{
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    landscape:{
+        color: "blue"
+    },
+    portrait:{
+        color: "red"
+    },
     child1:{
         backgroundColor: "green",
         minWidth: 100,
         minHeight: 100,
-        alignSelf: "center",
+        aspectRatio: 4/3, 
     },
     child2:{
         backgroundColor: "pink",
---
+++ src/shared/utils/testStyle.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+const testStyle = StyleSheet.create({
+    background: {
+        backgroundColor: "lightblue",
+    }
+})
+
+export default testStyle
